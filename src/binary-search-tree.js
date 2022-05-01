49b8d7e886737b69e6895053945b303547a924a3
Evgeniia <100534408+emoxowa@users.noreply.github.com>
2022-05-02 00:24:02 +0300

    check

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -18,29 +18,60 @@ class BinarySearchTree {
   add(data) {
     this.root_ = addWithin(this.root_, data)
     
-      function addWithin(node, data)  {
-        if (!node) {
-          return new Node(data);
-        }
-        if (node.data === data) {
-          return node;
-        }
-        if (data < node.data) {
-          node.left = addWithin(node.left, data);
-        } else {
-          node.right = addWithin(node.right, data);
-        }
+    function addWithin(node, data)  {
+      if (!node) {
+        return new Node(data);
+      }
+      if (node.data === data) {
         return node;
       }
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+      return node;
     }
+    
+  }
   
 
   has(data) {
-    
+    return searchWithin(this.root_, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return false;
+      }
+      if (node.data === data) {
+        return true;
+      }
+      if (data < node.data) {
+        return searchWithin(node.left, data);
+      } else {
+         return searchWithin(node.right, data);
+      }
+    }
+
   }
 
   find(data) {
-    
+    return searchWithin(this.root_, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (data < node.data) {
+        return searchWithin(node.left, data);
+      } else {
+        return searchWithin(node.right, data);
+      }
+    }
+
   }
 
   remove(data) {
@@ -48,11 +79,25 @@ class BinarySearchTree {
   }
 
   min() {
-    
+    if (!this.root_) {
+      return
+    } 
+    let node = this.root_;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    
+     if (!this.root_) {
+       return;
+     }
+     let node = this.root_;
+     while (node.right) {
+       node = node.right;
+     }
+     return node.data;   
   }
 }
 
