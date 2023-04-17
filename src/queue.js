09dfac920aaec0ff8355ae151b60d5e39467d698
Evgeniia <Kochetkovaev11@gmail.com>
2023-04-17 19:26:56 +0300

    feat queue

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,51 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
+class LinkedList {
   constructor() {
-    this.queue = [];
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  removeHead() {
+    if (!this.head) {
+      return null;
+    }
+    const removedHead = this.head;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return removedHead.value;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.linkedList = new LinkedList();
   }
 
   getUnderlyingList() {
-    return this.queue;
+    return this.linkedList.head;
   }
 
   enqueue(value) {
-    this.queue.push(value);
+    this.linkedList.append(value);
   }
 
   dequeue() {
-    return this.queue.shift();
+    return this.linkedList.removeHead();
   }
 }
 
